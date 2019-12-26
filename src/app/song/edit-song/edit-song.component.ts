befebca6097b7ba74a209f0cbc427bfa2f3c9ee8
dung-kieu <dungk344@gmail.com>
2019-12-26 08:42:25 +0700

    ...

--- src/app/song/edit-song/edit-song.component.ts
+++ src/app/song/edit-song/edit-song.component.ts
@@ -3,6 +3,7 @@ import {Router} from '@angular/router';
 import {SongService} from '../song.service';
 import {DataTransferService} from '../../data-transfer.service';
 import {Song} from '../Song';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-edit-song',
@@ -10,7 +11,31 @@ import {Song} from '../Song';
   styleUrls: ['./edit-song.component.scss']
 })
 export class EditSongComponent implements OnInit {
-  ngOnInit(): void {
+  songs: Song[];
+  song: Song;
+  songForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private songService: SongService,
+              private router: Router,
+              private dataTransfer: DataTransferService) {
   }
 
+  ngOnInit() {
+    this.song = this.dataTransfer.getData();
+    this.songForm = this.fb.group({
+      id: [this.song.id],
+      name: [this.song.name],
+      description: [this.song.description],
+      dateUpload: [this.song.dateUpLoad],
+      avatar: [this.song.avatar],
+      fileMp3: [this.song.fileMp3],
+    });
+  }
+
+
+  onSubmit() {
+    this.songService.editSong(this.songForm.value);
+    this.router.navigateByUrl('/');
+  }
 }
