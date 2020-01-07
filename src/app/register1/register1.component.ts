2a8ec5a29d4849f6116d17589ba707f17bc7936c
dung-kieu <dungk344@gmail.com>
2020-01-07 17:06:43 +0700

    Update: form login & register ....

---
+++ src/app/register1/register1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register1',
+  templateUrl: './register1.component.html',
+  styleUrls: ['./register1.component.scss']
+})
+export class Register1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
